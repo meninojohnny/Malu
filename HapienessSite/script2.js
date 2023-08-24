263a9fa7ef5ef3e19341a022126197f1b096728a
meninojohnny <120663121+meninojohnny@users.noreply.github.com>
2023-08-23 21:32:17 -0300

    Delete script2.js

--- HapienessSite/script2.js
+++
@@ -1,5 +0,0 @@
-
-
-function Gerar() {
-  foto.src  = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi0NVX0oEnxts6yJ_sZGW6B7OvGjWNe8W_0OBULB3uoFruOcK48jqMdczV8VlCp9_zMVgO7bkaOckcxwUY3xyNUwdWLhAL1g5TRkBk4Ru9LqfyUO5-DXmuYPUsEtS4aOrgrZ38glWyo6jGvQU_MmFCtkmwem1zZSPRQ1c4OrO5fI0K_xfXpr0HN-bRD4VaX/s980/Download%20Cute%20gray%20cat%20cartoon,%20vector%20illustration%20for%20free%20(1).jpeg';
-}
